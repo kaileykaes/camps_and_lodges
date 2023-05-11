a4bcc14527ace103d5944cf804ddd9db570ab627
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-11 12:11:47 -0600

    Test: format

--- spec/features/lodges/index_spec.rb
+++ spec/features/lodges/index_spec.rb
@@ -23,16 +23,15 @@ RSpec.describe 'Lodges' do
       scout_craft = dobbins.lodges.create!(name: 'Scout Craft', director: 'Lauren Dewey', number_of_staff: 4, specialty_area: false)
       silver_lake = dobbins.lodges.create!(name: 'Silver Lake', director: 'Jake Burr', number_of_staff: 6, specialty_area: true)
       visit "/lodges/#{scout_craft.id}"
-      expect(page).to have_content(scout_craft.name)
-      expect(page).to have_content(scout_craft.director)
-      expect(page).to have_content(scout_craft.number_of_staff)
-      expect(page).to have_content(scout_craft.specialty_area)
+      expect(page).to have_content("#{scout_craft.name}")
+      expect(page).to have_content("Director: #{scout_craft.director}")
+      expect(page).to have_content("Number of Staff: #{scout_craft.number_of_staff}")
+      expect(page).to have_content("Specialty Area: #{scout_craft.specialty_area}")
       visit "lodges/#{silver_lake.id}"
-      expect(page).to have_content(silver_lake.name)
-      expect(page).to have_content(silver_lake.director)
-      expect(page).to have_content(silver_lake.number_of_staff)
-      expect(page).to have_content(silver_lake.specialty_area)
-
+      expect(page).to have_content("#{silver_lake.name}")
+      expect(page).to have_content("Director: #{silver_lake.director}")
+      expect(page).to have_content("Number of Staff: #{silver_lake.number_of_staff}")
+      expect(page).to have_content("Specialty Area: #{silver_lake.specialty_area}")
     end
   end
 end

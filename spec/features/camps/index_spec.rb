607b3f153351b3cbe696ed1608591d75ce6fceaa
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-11 09:32:00 -0600

    test 'sees name of each parent record in system'

--- spec/features/camps/index_spec.rb
+++ spec/features/camps/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Camp index" do 
   it 'sees name of each parent record in system' do 
-    dobbins = Camp.create!(name: 'Camp Cris Dobbins', campground_number: 13, vacancy?: true)
+    dobbins = Camp.create!(name: 'Camp Cris Dobbins', campground_number: 13, vacancy: true)
     visit "/camps" 
     expect(page).to have_content(dobbins.name)
   end

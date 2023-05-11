9d5953deeaaba7bcf8fc4cb598b4da865b073aa7
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-11 11:39:08 -0600

    create migration to add lodges to camp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_11_173347) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_11_173836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_11_173347) do
     t.boolean "specialty_area"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "camp_id", null: false
+    t.index ["camp_id"], name: "index_lodges_on_camp_id"
   end
 
+  add_foreign_key "lodges", "camps"
 end

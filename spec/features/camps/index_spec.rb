3d954c2ddb09a224b05904ce2ce6a66991dbdc13
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-14 16:47:51 -0600

    Test: remove new.html.erb test from file

--- spec/features/camps/index_spec.rb
+++ spec/features/camps/index_spec.rb
@@ -64,20 +64,5 @@ RSpec.describe "Camp" do
       click_on "#{@dobbins.name} Lodges" 
       expect(current_path).to eq(camp_lodges_path(@dobbins))
     end
-
-    describe 'crud functionality' do 
-      it 'can add a new camp' do 
-        visit '/camps'
-        expect(page).to have_link('New Camp')
-        click_on 'New Camp'
-        expect(current_path).to eq('/camps/new')
-        fill_in 'Name', with: 'Philmont Scout Ranch'
-        fill_in 'Campground Number', with: 8
-        fill_in 'Vacancy', with: false
-        click_on 'Create Camp'
-        expect(current_path).to eq('/camps')
-        expect(page).to have_content('Philmont Scout Ranch')
-      end
-    end
   end
 end

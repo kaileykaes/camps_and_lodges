045827dfae30e3cda92a87a79487963e4202f3f6
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-14 19:26:49 -0600

    update?

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ('/')
   # root 'articles#index'
   get '/camps', to: 'camps#index'
+  get '/camps/new', to: 'camps#new'
+  post '/camps', to: 'camps#create'
   get '/camps/:id', to: 'camps#show'
-  get '/camps/:id/lodges', to: 'camps#show_lodges'
+  get '/camps/:id/lodges', to: 'camps#show_lodges', as: 'camp_lodges'
   get '/lodges', to: 'lodges#index'
   get '/lodges/:id', to: 'lodges#show'
 end

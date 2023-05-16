812a4e489ef7eb20b26e4d9aae0e2ca72b829121
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-16 16:57:26 -0600

    Test: sort_alphabetically ... or try, anyway

--- spec/models/camp_spec.rb
+++ spec/models/camp_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Camp, type: :model do 
+RSpec.describe Camp, type: :model do
+  before :each do 
+    @magness = Camp.create!(name: 'Magness Adventure Camp', campground_number: 4, vacancy: true)
+    @magness_lake = @magness.lodges.create!(name: 'Lake Area', director: 'Ian Hendrick', number_of_staff: 4, specialty_area: true)
+    @rock_range = @magness.lodges.create!(name: 'Rock Throwing Range', director: 'Chais Maranto', number_of_staff: 2, specialty_area: true)
+    @nature_lodge = @magness.lodges.create!(name: 'Nature Area', director: 'Stevo', number_of_staff: 3, specialty_area: false)
+  end
+
   it { should have_many :lodges }
+    
+  xit 'sort alphabetically' do 
+    expect(@magness.lodges.sort_alphabetically('yeahbaby')).to eq([@magness_lake, @nature_lodge, @rock_range])
+    expect(@magness.lodges.sort_alphabetically('nobaby')).to_not eq([@magness_lake, @nature_lodge, @rock_range])
+  end
 end

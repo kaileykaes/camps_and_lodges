fa52638436a30a462cdf23e1809ff722b9cac95c
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-16 12:44:14 -0600

    Feat: self.true_records

--- app/models/lodge.rb
+++ app/models/lodge.rb
@@ -1,3 +1,10 @@
 class Lodge < ApplicationRecord
   belongs_to :camp
+
+  def self.true_records 
+    lodges = Lodge.all
+    lodges.find_all do |lodge|
+      lodge.specialty_area == true
+    end
+  end
 end

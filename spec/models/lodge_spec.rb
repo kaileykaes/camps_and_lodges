4afa89886b17bc36351f1fc582bc143c5757ce5d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-16 12:43:48 -0600

    Test: true_records

--- spec/models/lodge_spec.rb
+++ spec/models/lodge_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Lodge, type: :model do 
   it { should belong_to :camp }
+
+  describe 'class methods' do 
+    before :each do 
+      @magness = Camp.create!(name: 'Magness Adventure Camp', campground_number: 4, vacancy: true)
+      @magness_lake = @magness.lodges.create!(name: 'Lake Area', director: 'Ian Hendrick', number_of_staff: 4, specialty_area: true)
+      @rock_range = @magness.lodges.create!(name: 'Rock Throwing Range', director: 'Chais Maranto', number_of_staff: 2, specialty_area: true)
+      @nature_lodge = @magness.lodges.create!(name: 'Nature Area', director: 'Stevo', number_of_staff: 3, specialty_area: false)
+    end
+
+    it 'sorts truthy records' do 
+      expect(Lodge.true_records).to eq([@magness_lake, @rock_range])
+    end
+  end
 end
